c25c14d061dfcf9ec667962637ab6211b3dc9911
zuiqiangqishao <727040917@qq.com>
2018-05-23 11:51:10 +0800

    昨天回去复习了下C，该进了下代码，没用全局变量了

--- 1、两数之和/a.c
+++ 1、两数之和/a.c
@@ -10,8 +10,9 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].*/
 
 #include <stdio.h>
+#include <stdlib.h>
 int* twoSum(int* nums, int numsSize, int target);
-int return_arr[2];
+
 int main()
 {
 	int test[4] = { 2,7,11,15};
@@ -24,7 +25,7 @@ int main()
 int* twoSum(int* nums, int numsSize, int target) {
 	//两个数组中每两个数字相加一次即可
 	int i, j;
-
+        int *return_arr = (int *) malloc(2 * sizeof(int));
 	for (i = 0; i<numsSize; i++) {
 		for (j = i + 1; j< numsSize; j++) {
 			if (nums[i] + nums[j] == target) {
