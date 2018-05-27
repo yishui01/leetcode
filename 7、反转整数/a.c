c5e0f0334a99d7944f071303e35df9609d4dc5c9
zuiqiangqishao <727040917@qq.com>
2018-05-27 10:36:53 +0800

    优化了逐个取数字的算法

--- 7、反转整数/a.c
+++ 7、反转整数/a.c
@@ -1,42 +1,29 @@
 int reverse(int x) {
-    //先确定整数有几位
-    int len = 1, res = 0, copy;
-    copy = x;
-    if (x <0){
-        if(-x < 0) {
-            return 0;
-        }
-        copy = -x;
-    }
- 
-    while((copy / 10) > 0) {
-        len ++;
-        copy = copy /10;
-        
-    }
    
-    if(len == 1) {
-        return x;
+    int i = 0, res = 0, len = 0, copy;
+    copy = x;
+    while (copy != 0) {
+        copy /= 10;
+        len++;
     }
     
-    int i = 0, curren = 0, pre = 0, s;
-    for (i = 0; i < len; i++) {
-        s = x / pow(10, len-1-i);
-        curren = s-pre;
-        pre = s*10;
-        if(x > 0) {
-            if (pow(2,31)-1-res < curren*pow(10,i)){
-                return 0;
-            }
+    while(x != 0) {
+        if (x > 0) {
+             if(res + x % 10 * pow(10, len-i-1) > pow(2, 31) -1){
+                 return 0;
+             }
         } else {
-            if (pow(-2,31)-1-res > curren*pow(10,i)){
-                return 0;
-            }
+             if(res + x % 10 * pow(10, len-i-1) < pow(-2, 31)){
+             return 0;
+         }
         }
-        res += curren*pow(10,i);
+        res += x % 10 * pow(10, len-i-1);
+        x = x / 10;
+        i++;
     }
-
+    
     return res;
+   
     
     
 }
