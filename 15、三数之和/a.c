32414f8d1caa94d7ef316ea2c5fb4a5676aa39be
zuiqiangqishao <727040917@qq.com>
2018-06-04 23:04:47 +0800

    这题有点难，毫无头绪

---
+++ 15、三数之和/a.c
@@ -0,0 +1,11 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int** threeSum(int* nums, int numsSize, int* returnSize) {
+    /*这道题看完毫无头绪，借鉴了网上的解题思路，先对数组进行排序，然后确定一个值，接下来对后面的数采用双指针，两者加起来=确定的值的相反数即可*/
+    /*数组为从小到大排序，双指针一左一右只想确定的值之后的序列的两个端点，值大于target时，右指针左移，小于target时左指针右移，还要进行去重复操作*/
+    /*代码明天再写吧，太晚了*/
+    
+    
+}
