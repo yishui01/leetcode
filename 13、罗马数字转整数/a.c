fab2b9dc4af54e3803c52e6ccc7dc6cf84ecb24c
zuiqiangqishao <727040917@qq.com>
2018-06-08 16:06:04 +0800

    把elseif替换成switch

--- 13、罗马数字转整数/a.c
+++ 13、罗马数字转整数/a.c
@@ -1,39 +1,48 @@
 int romanToInt(char* s) {
     int res = 0;
     while(*s) {
-        if(*s == 'I') {
-            res++;
-        } else if(*s == 'X') {
-            res+=10;
-            if(*(s-1) == 'I') {
-                res -= 2;
-            }
-        } else if(*s == 'C') {
-            res+=100;
-            if(*(s-1) == 'X') {
-                res -= 20;
-            }
-        } else if(*s == 'M') {
-            res+=1000;
-            if(*(s-1) == 'C') {
-                res -= 200;
-            }
-        } else if(*s == 'V') {
-            res+=5;
-            if(*(s-1) == 'I') {
-                res -= 2;
-            }
-        } else if(*s == 'L') {
-            res +=50;
-            if(*(s-1) == 'X') {
-                res -= 20;
-            }
-        } else if(*s == 'D') {
-            res += 500;
-            if(*(s-1) == 'C') {
-                res -= 200;
-            }
+        switch(*s) {
+            case 'I':
+                res++;
+                break;
+            case 'X':
+                 res+=10;
+                if(*(s-1) == 'I') {
+                    res -= 2;
+                }
+                break;
+            case 'C':
+                 res+=100;
+                if(*(s-1) == 'X') {
+                    res -= 20;
+                }
+                break;
+            case 'M':
+                res+=1000;
+                if(*(s-1) == 'C') {
+                    res -= 200;
+                }
+                break;
+            case 'V':
+                res+=5;
+                if(*(s-1) == 'I') {
+                    res -= 2;
+                }
+                break;
+            case 'L':
+                res +=50;
+                if(*(s-1) == 'X') {
+                    res -= 20;
+                }
+                break;
+            case 'D':
+                res += 500;
+                if(*(s-1) == 'C') {
+                    res -= 200;
+                }
+                break;
         }
+
         s++;
     }
     
