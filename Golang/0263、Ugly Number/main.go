e6b439c0c645d936404c7c966a4a1a3276c6a3f9
zuiqiangqishao <727040917@qq.com>
2019-10-25 07:54:09 +0800

    丑数，直接三个for循环取模，模到不能等于0为止

---
+++ Golang/0263、Ugly Number/main.go
@@ -0,0 +1,16 @@
+func isUgly(num int) bool {
+    if num < 1 {
+        return false
+    }
+    for num % 2 == 0 {
+        num /= 2
+    }
+    for num % 3 == 0 {
+        num /= 3
+    }
+    for num % 5 == 0 {
+        num /= 5
+    }
+    
+    return num == 1
+}
